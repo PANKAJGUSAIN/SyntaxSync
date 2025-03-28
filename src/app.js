7f7cda3f4aebbc9306ed61975fe2448c30078167
Pankaj Gusain <gusainpankaj1999@gmail.com>
2025-03-28 23:06:43 +0530

    implement signup, user retrieval, and feed endpoints

--- src/app.js
+++ src/app.js
@@ -2,42 +2,66 @@ require("dotenv").config(); // Load environment variables
 
 const express = require("express");
 const app = express();
-const {connectdb} = require('./config/database');
-const {User} = require('./models/user');
+const { connectdb } = require('./config/database');
+const { User } = require('./models/user');
 
 app.use(express.json()); // to convert json in a javascript object 
 
-
-app.post("/signup" , async(req ,res)=>{
+// signup api
+app.post("/signup", async (req, res) => {
     console.log(req.body);
 
     // create an instance of the usermodel
     const user = new User(req.body);
-    try{
+    try {
         const result = await user.save();
         console.log(result);
         res.send("user added successfully !")
-    }catch(err){
-        console.log("Error occured when adding user " , err.message);
+    } catch (err) {
+        console.log("Error occured when adding user ", err.message);
     }
-    
-    
 
+})
+
+// find a specific user 
+app.get("/user", async (req, res) => {
+    try {
+        const result = await User.find({ emailId: req.body.email });
+        if (!result) {
+            res.status(404).send("User not Found");
+        }
+        res.status(200).send(result);
+    } catch (err) {
+        res.status(404).send("User not Found");
+        console.log("Error occured when fetching  user ", err.message);
+    }
+
+})
+
+// find all users 
+app.get("/feed", async (req, res) => {
+    try {
+        const result = await User.find({});
+        res.status(200).send(result);
+    } catch (err) {
+        res.status(404).send("User not Found");
+        console.log("Error occured when fetching  user ", err.message);
+    }
 
 })
 
 
 // to make sure database is connected before the server starts to listen 
 connectdb()
-.then(()=>{
-    console.log("Database connected successfully...");
-    app.listen(3000 , ()=>{
-        console.log(`successfully listening to port ${3000}`);
+    .then(() => {
+        console.log("Database connected successfully...");
+        app.listen(3000, () => {
+            console.log(`successfully listening to port ${3000}`);
+        });
+    })
+    .catch((err) => {
+        console.error("Database cannot be connected!!")
     });
-})
-.catch((err)=>{
-    console.error("Database cannot be connected!!")
-});
 
 
 
